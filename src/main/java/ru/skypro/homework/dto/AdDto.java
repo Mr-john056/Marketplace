d48caa40087518a2497bbc1ec8e75e78b39d7a7d
19kuz <19kuzminykh78@mail.ru>
2024-05-19 20:34:32 +0800

    Сделан CommentDto.

--- src/main/java/ru/skypro/homework/dto/AdDto.java
+++ src/main/java/ru/skypro/homework/dto/AdDto.java
@@ -3,7 +3,7 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 
 @Data
-public class Ad {
+public class AdDto {
     private int author;
     private String image;
     private int pk;
--- src/main/java/ru/skypro/homework/dto/AdsDto.java
+++ src/main/java/ru/skypro/homework/dto/AdsDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class Ads {
+public class AdsDto {
     private int count;
-    private List<Ad> results;
+    private List<AdDto> results;
 }
