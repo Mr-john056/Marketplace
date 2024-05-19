e144699b13d3fad0479fe6e854a5e7afc8e4d199
19kuz <19kuzminykh78@mail.ru>
2024-05-19 21:04:37 +0800

    Сделан LoginDto.

--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.homework.dto.Login;
+import ru.skypro.homework.dto.LoginDto;
 import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.service.AuthService;
 
@@ -21,7 +21,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Login login) {
+    public ResponseEntity<?> login(@RequestBody LoginDto login) {
         if (authService.login(login.getUsername(), login.getPassword())) {
             return ResponseEntity.ok().build();
         } else {
--- src/main/java/ru/skypro/homework/dto/LoginDto.java
+++ src/main/java/ru/skypro/homework/dto/LoginDto.java
@@ -3,8 +3,7 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 
 @Data
-public class Login {
-
-    private String username;
+public class LoginDto {
     private String password;
+    private String username;
 }
