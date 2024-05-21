32e3a2810d6df22adf95aef293b7dae0e0f1fef3
19kuz <19kuzminykh78@mail.ru>
2024-05-21 21:59:05 +0800

    Сделан репозиторий UserRepository.

--- src/main/java/ru/skypro/homework/repositories/UserRepository.java
+++ src/main/java/ru/skypro/homework/repositories/UserRepository.java
@@ -2,7 +2,7 @@ package ru.skypro.homework.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import ru.skypro.homework.entity.User;
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
