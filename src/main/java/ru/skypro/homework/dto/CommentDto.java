3dc96e821192240b2450a3083fd84fdc5bf47b15
19kuz <19kuzminykh78@mail.ru>
2024-05-21 21:20:05 +0800

    Сделаны сущьности Comment и Ad.

--- src/main/java/ru/skypro/homework/dto/CommentDto.java
+++ src/main/java/ru/skypro/homework/dto/CommentDto.java
@@ -7,7 +7,7 @@ public class CommentDto {
     private int author;
     private String authorImage;
     private String authorFirstName;
-    private int createdAt;
+    private int createdAd;
     private int pk;
     private String text;
 }
