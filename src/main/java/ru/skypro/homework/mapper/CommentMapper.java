e02e64b38fd9b08ccbdeac1f850cd8db8c8b0e3d
19kuz <19kuzminykh78@mail.ru>
2024-05-23 22:13:36 +0800

    Сделан CommentMapper.

---
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -0,0 +1,42 @@
+package ru.skypro.homework.mapper;
+
+import ru.skypro.homework.dto.CommentDto;
+import ru.skypro.homework.dto.CommentsDto;
+import ru.skypro.homework.dto.CreateOrUpdateCommentDto;
+import ru.skypro.homework.entity.Comment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CommentMapper {
+    public Comment toEntity (CreateOrUpdateCommentDto createOrUpdateCommentDto){
+        Comment comment = new Comment();
+        comment.setText(createOrUpdateCommentDto.getText());
+        return comment;
+    }
+
+    public CommentDto toCommentDto(Comment comment){
+
+        CommentDto commentDto = new CommentDto();
+        commentDto.setPk(comment.getPk());
+        commentDto.setCreatedAd(comment.getCreatedAd());
+        commentDto.setText(comment.getText());
+        commentDto.setAuthor(comment.getUser().getId());
+        commentDto.setAuthorFirstName(comment.getUser().getFirstName());
+        commentDto.setAuthorImage(comment.getUser().getImage());
+
+        return commentDto;
+    }
+
+    public CommentsDto toCommentsDto(List<Comment> comments) {
+        CommentsDto commentsDto = new CommentsDto();
+        List<CommentDto> commentDtoList = comments.stream()
+                .map(this::toCommentDto)
+                .collect(Collectors.toList());
+
+        commentsDto.setCount(commentDtoList.size());
+        commentsDto.setResults(commentDtoList);
+
+        return commentsDto;
+    }
+}
