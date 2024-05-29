052e66fbf3bdf34bd5b9d4101e883696cf719c8c
19kuz <19kuzminykh78@mail.ru>
2024-05-29 23:37:35 +0800

    Создан класс MyUserDetailsService.

--- src/main/java/ru/skypro/homework/repositories/UserRepository.java
+++ src/main/java/ru/skypro/homework/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package ru.skypro.homework.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.User;
+
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<Object> findByEmail(String email);
 }
--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -1,47 +1,49 @@
 package ru.skypro.homework.service.impl;
 
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import ru.skypro.homework.config.MyUserDetails;
+import ru.skypro.homework.config.MyUserDetailsService;
+import ru.skypro.homework.dto.LoginDto;
 import ru.skypro.homework.dto.RegisterDto;
+import ru.skypro.homework.repositories.UserRepository;
 import ru.skypro.homework.service.AuthService;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
-    private final UserDetailsManager manager;
+    private final MyUserDetailsService myUserDetailService;
     private final PasswordEncoder encoder;
+    private final UserRepository userRepository;
 
-    public AuthServiceImpl(UserDetailsManager manager,
-                           PasswordEncoder passwordEncoder) {
-        this.manager = manager;
+    public AuthServiceImpl(MyUserDetailsService myUserDetailService,
+                           PasswordEncoder passwordEncoder,
+                           UserRepository userRepository) {
+        this.myUserDetailService = myUserDetailService;
         this.encoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
 
     @Override
     public boolean login(String userName, String password) {
-        if (!manager.userExists(userName)) {
-            return false;
-        }
-        UserDetails userDetails = manager.loadUserByUsername(userName);
-        return encoder.matches(password, userDetails.getPassword());
+        return false;
     }
 
     @Override
-    public boolean register(RegisterDto register) {
-        if (manager.userExists(register.getUsername())) {
+    public boolean login(LoginDto loginDto) {
+
+        MyUserDetails userDetails = myUserDetailService.loadUserByUsername(loginDto.getUsername());
+        return encoder.matches(loginDto.getPassword(), userDetails.getPassword());
+    }
+
+    @Override
+    public boolean register(RegisterDto registerDto) {
+        if(userRepository.findByEmail(registerDto.getUsername()).isPresent()) {
             return false;
+        } else {
+            //userService.registerUser(registerDto);
+            return true;
         }
-        manager.createUser(
-                User.builder()
-                        .passwordEncoder(this.encoder::encode)
-                        .password(register.getPassword())
-                        .username(register.getUsername())
-                        .roles(register.getRole().name())
-                        .build());
-        return true;
     }
 
 }
