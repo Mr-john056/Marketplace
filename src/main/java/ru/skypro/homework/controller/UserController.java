496b1d3578f2d6faa747cb29d387be918275d669
19kuz <19kuzminykh78@mail.ru>
2024-05-31 19:38:41 +0800

    Исправлен UserController.

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -9,31 +9,51 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDto;
 import ru.skypro.homework.dto.UpdateUserDto;
 import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.exception.PasswordIsNotCorrectException;
+import ru.skypro.homework.service.impl.UserServiceImpl;
 
+import javax.validation.Valid;
 import java.io.IOException;
 
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/set_password")
-    public ResponseEntity<NewPasswordDto> setPassword(@RequestBody NewPasswordDto dto, Authentication authentication) {
+    public ResponseEntity<NewPasswordDto> setPassword(@Valid @RequestBody NewPasswordDto dto, Authentication authentication) {
+        try {
+            userService.updatePassword(dto, authentication.getName());
+        } catch (PasswordIsNotCorrectException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(dto);
+        }
+
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping("/me")
     public ResponseEntity<UserDto> getUserInfo(Authentication authentication) {
-        return ResponseEntity.ok(new UserDto());
+        return ResponseEntity.ok(userService.getInfoAboutMe(authentication.getName()));
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<UpdateUserDto> updateUser(@RequestBody UpdateUserDto dto, Authentication authentication) {
-        return ResponseEntity.ok(dto);
+    public ResponseEntity<UpdateUserDto> updateUser(@Valid @RequestBody UpdateUserDto dto, Authentication authentication) {
+        return ResponseEntity.ok(userService.updateInfoAboutMe(authentication.getName(), dto));
     }
 
     @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> updateUserImage(@RequestParam MultipartFile image, Authentication authentication) throws IOException {
+        userService.updateMyImage(authentication.getName(), image);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping(value = "{username}/image", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+    public ResponseEntity<byte[]> getUserImage(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getImage(username));
     }
 }
